e5e89981471ff7364aab7dd8988671f1fafac1e0
Nickolodeon98 <sjeon0730@gmail.com>
2022-11-10 11:41:35 +0900

    feat: 컨트롤러에 주어진 id 로 레코드를 찾고 이를 토대로 show 페이지에 정보를 띄워주는 selectSingleRecord() 메서드 추가

--- src/main/java/com/practice/springbootdocker/controller/DockerBoardController.java
+++ src/main/java/com/practice/springbootdocker/controller/DockerBoardController.java
@@ -5,9 +5,13 @@ import com.practice.springbootdocker.domain.entity.DockerBoard;
 import com.practice.springbootdocker.repository.DockerBoardRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/notice")
@@ -32,4 +36,15 @@ public class DockerBoardController {
         return "";
     }
 
+    @GetMapping("/{id}")
+    public String selectSingleRecord(@PathVariable Long id, Model model) {
+        Optional<DockerBoard> optDockerBoard = dockerBoardRepository.findById(id);
+        if (optDockerBoard.isPresent()) {
+            model.addAttribute("record", optDockerBoard);
+            return "dockerboard/show";
+        } else {
+            model.addAttribute("message", String.format("id %d를 찾을 수 없습니다", id));
+            return "error";
+        }
+    }
 }
