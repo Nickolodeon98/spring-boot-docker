f276807d8f23bc70b3a713097ef242c7b7c58a63
Nickolodeon98 <sjeon0730@gmail.com>
2022-11-10 11:15:16 +0900

    feat: DockerBoard DTO 에 엔티티 변환 메서드와 멤버 변수, 생성자, Getter 추가

--- src/main/java/com/practice/springbootdocker/domain/dto/DockerBoardDto.java
+++ src/main/java/com/practice/springbootdocker/domain/dto/DockerBoardDto.java
@@ -1,4 +1,19 @@
 package com.practice.springbootdocker.domain.dto;
 
+import com.practice.springbootdocker.domain.entity.DockerBoard;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Getter
+@NoArgsConstructor
 public class DockerBoardDto {
+    private Long id;
+    private String title;
+    private String contents;
+    private String author;
+    public DockerBoard toEntity() {
+        return new DockerBoard(this.id, this.title, this.contents, this.author);
+    }
 }
