2626029119b611af304f558525bda7d1b9c55642
Nickolodeon98 <sjeon0730@gmail.com>
2022-11-10 11:12:46 +0900

    feat: DockerBoard 엔티티 멤버 변수 및 생성자 추가

--- src/main/java/com/practice/springbootdocker/domain/entity/DockerBoard.java
+++ src/main/java/com/practice/springbootdocker/domain/entity/DockerBoard.java
@@ -1,4 +1,21 @@
 package com.practice.springbootdocker.domain.entity;
 
+import lombok.AllArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@AllArgsConstructor
 public class DockerBoard {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String contents;
+    private String author;
 }
