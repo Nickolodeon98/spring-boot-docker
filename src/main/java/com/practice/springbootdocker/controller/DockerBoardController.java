446a7cf135927a78a5bc1f105a39c4cb75d6ecb3
Nickolodeon98 <sjeon0730@gmail.com>
2022-11-10 11:24:44 +0900

    feat: 컨트롤러에 폼으로 정보를 받아 새로운 항목을 DB 테이블에 넣을 수 있게 하는 GET 과 POST 메서드 추가

--- src/main/java/com/practice/springbootdocker/controller/DockerBoardController.java
+++ src/main/java/com/practice/springbootdocker/controller/DockerBoardController.java
@@ -1,4 +1,35 @@
 package com.practice.springbootdocker.controller;
 
+import com.practice.springbootdocker.domain.dto.DockerBoardDto;
+import com.practice.springbootdocker.domain.entity.DockerBoard;
+import com.practice.springbootdocker.repository.DockerBoardRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/notice")
+@Slf4j
 public class DockerBoardController {
+
+    private final DockerBoardRepository dockerBoardRepository; // DI 해준다.
+
+    public DockerBoardController(DockerBoardRepository dockerBoardRepository) {
+        this.dockerBoardRepository = dockerBoardRepository;
+    }
+
+    @GetMapping("/new")
+    public String createNewPostForm() {
+        return "dockerboard/new";
+    }
+
+    @PostMapping("/post")
+    public String createNewPost(DockerBoardDto dockerBoardDto) {
+        log.info("Title:{} Contents:{} Author:{}", dockerBoardDto.getTitle(), dockerBoardDto.getContents(), dockerBoardDto.getAuthor());
+        DockerBoard savedDockerBoard = dockerBoardRepository.save(dockerBoardDto.toEntity());
+        return "";
+    }
+
 }
