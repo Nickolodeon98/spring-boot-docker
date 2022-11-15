13901006ae13449a04fc7e6d751d4d9864058a63
Nickolodeon98 <sjeon0730@gmail.com>
2022-11-15 14:07:05 +0900

    feat: HospitalResponse 에 로직에 따라 새로운 정보를 추가할 수 있는 setter 추가

--- src/main/java/com/practice/springbootdocker/domain/dto/HospitalResponse.java
+++ src/main/java/com/practice/springbootdocker/domain/dto/HospitalResponse.java
@@ -3,7 +3,6 @@ package com.practice.springbootdocker.domain.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@AllArgsConstructor
 @Getter
 public class HospitalResponse {
 
@@ -14,8 +13,24 @@ public class HospitalResponse {
     private Integer totalNumberOfBeds;
     private String businessTypeName;
     private Float totalAreaSize;
+    private String businessStatusName;
 
-//    public HospitalResponse(Integer id, String hospitalName) {
+    public HospitalResponse(Integer id, String roadNameAddress, String hospitalName, Integer patientRoomCount,
+                            Integer totalNumberOfBeds, String businessTypeName, Float totalAreaSize) {
+        this.id = id;
+        this.roadNameAddress = roadNameAddress;
+        this.hospitalName = hospitalName;
+        this.patientRoomCount = patientRoomCount;
+        this.totalNumberOfBeds = totalNumberOfBeds;
+        this.businessTypeName = businessTypeName;
+        this.totalAreaSize = totalAreaSize;
+    }
+
+    public void setBusinessStatusName(String businessStatusName) {
+        this.businessStatusName = businessStatusName;
+    }
+
+    //    public HospitalResponse(Integer id, String hospitalName) {
 //        this.id = id;
 //        this.hospitalName = hospitalName;
 //    }
