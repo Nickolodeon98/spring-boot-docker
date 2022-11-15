2afda2929f333001fda7e0db0a505a4a380fc093
Nickolodeon98 <sjeon0730@gmail.com>
2022-11-15 15:38:48 +0900

    feat: HospitalRestControllerTest 를 @WebMvcTest 사용하여 작성, 편의를 위해 HospitalResponse 에 @Builder 추가

--- src/main/java/com/practice/springbootdocker/domain/dto/HospitalResponse.java
+++ src/main/java/com/practice/springbootdocker/domain/dto/HospitalResponse.java
@@ -1,9 +1,12 @@
 package com.practice.springbootdocker.domain.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
+@Builder // 객체 생성 시 하나하나 보면서 순서대로 매개 변수를 넣지 않아도 생성에 무리가 없게 해준다. 멤버 변수 이름 별로 지정이 가능해지기 때문.
 @Getter
+@AllArgsConstructor
 public class HospitalResponse {
 
     private Integer id;
---
+++ src/test/java/com/practice/springbootdocker/controller/HospitalRestControllerTest.java
@@ -0,0 +1,65 @@
+package com.practice.springbootdocker.controller;
+
+import com.practice.springbootdocker.domain.dto.HospitalResponse;
+import com.practice.springbootdocker.service.HospitalService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(HospitalRestController.class)
+public class HospitalRestControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    /* MockBean 을 사용함으로서 가짜 객체를 생성해주고 DB 와 상관 없이 테스트가 가능해진다.
+     * 그러므로 @Autowired 하위에 있는 어노테이션이 아니다. */
+    @MockBean
+    HospitalService hospitalService;
+
+    /* Service 가 아니라 Controller 만 검증하는 테스트이다. */
+    @Test
+    @DisplayName("Json 형태로 잘 반환하는지")
+    void jsonResponse() throws Exception {
+        /* {"id":2321,"roadNameAddress":"서울특별시 서초구 서초중앙로 230, 202호 (반포동, 동화반포프라자빌딩)",
+        "hospitalName":"노소아청소년과의원","patientRoomCount":0,"totalNumberOfBeds":0,"businessTypeName":"의원",
+        "totalAreaSize":0.0,"businessStatusName":"영업중"} */
+        HospitalResponse hospitalResponse = HospitalResponse.builder()
+                .id(2321)
+                .roadNameAddress("서울특별시 서초구 서초중앙로 230, 202호 (반포동, 동화반포프라자빌딩)")
+                .hospitalName("노소아청소년과의원")
+                .patientRoomCount(0)
+                .totalNumberOfBeds(0)
+                .businessTypeName("의원")
+                .totalAreaSize(0.0f)
+                .businessStatusName("영업중")
+                .build();
+
+        given(hospitalService.hospitalResponse(2321)).willReturn(hospitalResponse); // WebMvcTest 하위 메서드인지?
+
+        Integer hospitalId = 2321;
+
+        String url = String.format("/api/v1/hospital/response/%d", hospitalId);
+        mockMvc.perform(get(url))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.hospitalName").exists())
+                .andExpect(jsonPath("$.hospitalName").value("노소아청소년과의원"))
+                .andDo(print()); // http request 와 response 내용을 출력한다.
+
+        verify(hospitalService).hospitalResponse(hospitalId);
+
+    }
+
+
+
+}
