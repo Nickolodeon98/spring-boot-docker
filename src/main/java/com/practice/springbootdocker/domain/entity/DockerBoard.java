d7e5d2ab63ec51ab562b5cbae621dfbef461b897
Nickolodeon98 <sjeon0730@gmail.com>
2022-11-16 15:52:17 +0900

    feat: DockerBoard 엔티티에서 DTO 로 변환하는 메서드들 추가

--- src/main/java/com/practice/springbootdocker/domain/entity/DockerBoard.java
+++ src/main/java/com/practice/springbootdocker/domain/entity/DockerBoard.java
@@ -1,18 +1,17 @@
 package com.practice.springbootdocker.domain.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import com.practice.springbootdocker.domain.dto.DockerBoardAddResponse;
+import com.practice.springbootdocker.domain.dto.DockerBoardResponse;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Builder
 @AllArgsConstructor
 @Getter
-@NoArgsConstructor
 @Table(name="docker_board")
 public class DockerBoard {
 
@@ -31,6 +30,22 @@ public class DockerBoard {
         this.author = author;
     }
 
+    public DockerBoard() {
+    }
+
     @OneToMany(mappedBy = "postToComment")
     private List<Comment> comments = new ArrayList<>();
+
+    public static DockerBoardResponse of(DockerBoard dockerBoard) {
+        return new DockerBoardResponse(dockerBoard.getId(), dockerBoard.getTitle(), dockerBoard.getContents(), dockerBoard.getAuthor());
+    }
+
+    public static DockerBoardAddResponse addResponseOf(DockerBoard dockerBoard) {
+        return DockerBoardAddResponse.builder()
+                .id(dockerBoard.getId())
+                .title(dockerBoard.getTitle())
+                .contents(dockerBoard.getContents())
+                .author(dockerBoard.getAuthor())
+                .build();
+    }
 }
