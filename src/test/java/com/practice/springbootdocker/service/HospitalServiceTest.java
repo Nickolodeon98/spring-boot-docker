fef1c1b695ae6a506e9537eec2597f3764c2a1da
Nickolodeon98 <sjeon0730@gmail.com>
2022-11-18 11:45:03 +0900

    feat: HospitalService 코드에 따른 영업중, 폐업 분류 테스트를 하나로 작성

--- src/test/java/com/practice/springbootdocker/service/HospitalServiceTest.java
+++ src/test/java/com/practice/springbootdocker/service/HospitalServiceTest.java
@@ -26,16 +26,23 @@ class HospitalServiceTest {
     @DisplayName("코드를 보고 영업중과 폐업 상태를 구분한다.")
     void distinguishCode() {
 
-        Optional<Hospital> hospital = Optional.of(Hospital.builder()
+        Optional<Hospital> openHospital = Optional.of(Hospital.builder()
                         .hospitalName("의원")
                         .id(1)
                         .businessStatusCode(13)
                         .businessTypeName("피부과")
                         .build());
 
-        Mockito.when(hospitalRepository.findById(1)).thenReturn(hospital);
+        Optional<Hospital> closedHospital = Optional.of(Hospital.builder()
+                        .id(23)
+                        .businessStatusCode(3)
+                        .build());
+
+        Mockito.when(hospitalRepository.findById(1)).thenReturn(openHospital);
+        Mockito.when(hospitalRepository.findById(23)).thenReturn(closedHospital);
 
         assertEquals("영업중", hospitalService.hospitalResponse(1).getBusinessStatusName());
+        assertEquals("폐업", hospitalService.hospitalResponse(23).getBusinessStatusName());
     }
 
 
